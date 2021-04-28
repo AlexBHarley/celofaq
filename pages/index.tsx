3503a4c2c81a84739b659cf206ffda122d55eb3c
Alex Harley <alexbruceharley@gmail.com>
2021-04-28 15:51:49 +0200

    chore: collapsible

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import ReactCollapsible from 'react-collapsible';
+import { ChevronRightIcon, ChevronDownIcon } from '@heroicons/react/solid';
+
+function Collapsible({ title, children }: any) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ReactCollapsible
+      trigger={
+        <h2 className="flex items-center space-x-3">
+          {open ? (
+            <ChevronDownIcon className="h-6 w-6" />
+          ) : (
+            <ChevronRightIcon className="h-6 w-6" />
+          )}
+          <span>{title}</span>
+        </h2>
+      }
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+    >
+      {children}
+    </ReactCollapsible>
+  );
+}
+
 export default function Home() {
   return (
     <div className="max-w-screen-sm mx-auto py-10 prose">
@@ -15,113 +42,122 @@ export default function Home() {
         .
       </p>
 
-      <h2>What makes Celo special?</h2>
-      <p>Coming soon...</p>
+      <Collapsible title="What makes Celo special?">
+        <p>Coming soon...</p>
+      </Collapsible>
 
-      <h2>What are CELO, cUSD and cEUR?</h2>
-      <p>
-        cUSD and cEUR are stable coins on the Celo network. They track the price
-        1-to-1 of their real world counterparts. Stable coins are important to a
-        blockchain platform as it enables users to send and receive amounts that
-        aren't subject to large fluctuations in price.
-      </p>
-      <p>
-        CELO is the platforms native asset and is used to vote for validator
-        groups (staking) and vote on governance proposals.
-      </p>
+      <Collapsible title="What are CELO, cUSD and cEUR?">
+        <p>
+          cUSD and cEUR are stable coins on the Celo network. They track the
+          price 1-to-1 of their real world counterparts. Stable coins are
+          important to a blockchain platform as it enables users to send and
+          receive amounts that aren't subject to large fluctuations in price.
+        </p>
+        <p>
+          CELO is the platforms native asset and is used to vote for validator
+          groups (staking) and vote on governance proposals.
+        </p>
+      </Collapsible>
 
-      <h2>Buying CELO</h2>
-      <p>
-        You can buy CELO, cUSD and cEUR at most major exchanges,
-        <ul>
-          <li>
-            <a target="_blank" href="https://binance.com">
-              Binance
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://coinbase.com">
-              Coinbase
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://bittrex.com">
-              Bittrex
-            </a>
-          </li>
-        </ul>
-        to name a few. For a more in depth look, see the{' '}
-        <a
-          target="_blank"
-          href="https://coinmarketcap.com/currencies/celo/markets/"
-        >
-          Coin Market Cap exchanges overview
-        </a>
-        .
-      </p>
+      <h2></h2>
+      <Collapsible title="Buying CELO">
+        <p>
+          You can buy CELO, cUSD and cEUR at most major exchanges,
+          <ul>
+            <li>
+              <a target="_blank" href="https://binance.com">
+                Binance
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://coinbase.com">
+                Coinbase
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://bittrex.com">
+                Bittrex
+              </a>
+            </li>
+          </ul>
+          to name a few. For a more in depth look, see the{' '}
+          <a
+            target="_blank"
+            href="https://coinmarketcap.com/currencies/celo/markets/"
+          >
+            Coin Market Cap exchanges overview
+          </a>
+          .
+        </p>
+      </Collapsible>
 
-      <h2>Staking</h2>
-      <p>
-        Staking is the act of voting for a validator group in the Celo network.
-        Once a validator group has enough votes (one CELO equals one vote) they
-        are able to elect individual validators to run on the Celo network.
-      </p>
-      <p>
-        Staking enables users to earn up to 5.5% APY for their investment.
-        Rewards are paid out daily.
-      </p>
-      <p>
-        You can currently stake via the following methods:
-        <ul>
-          <li>
-            <a target="_blank" href="https://plock.fi">
-              Plock.fi
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://celowallet.app">
-              Celo Wallet
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://celoterminal.com">
-              Celo Terminal
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://docs.celo.org/command-line-interface/introduction"
-            >
-              Celo CLI
-            </a>{' '}
-            - for advanced users only.
-          </li>
-        </ul>
-      </p>
+      <Collapsible title="Staking">
+        <p>
+          Staking is the act of voting for a validator group in the Celo
+          network. Once a validator group has enough votes (one CELO equals one
+          vote) they are able to elect individual validators to run on the Celo
+          network.
+        </p>
+        <p>
+          Staking enables users to earn up to 5.5% APY for their investment.
+          Rewards are paid out daily.
+        </p>
+        <p>
+          You can currently stake via the following methods:
+          <ul>
+            <li>
+              <a target="_blank" href="https://plock.fi">
+                Plock.fi
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://celowallet.app">
+                Celo Wallet
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://celoterminal.com">
+                Celo Terminal
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://docs.celo.org/command-line-interface/introduction"
+              >
+                Celo CLI
+              </a>{' '}
+              - for advanced users only.
+            </li>
+          </ul>
+        </p>
+      </Collapsible>
 
-      <h2>Help! I sent CELO to my Ethereum address</h2>
-      <p>
-        See this guide{' '}
-        <a
-          target="_blank"
-          href="https://docs.celo.org/celo-owner-guide/eth-recovery"
-        >
-          here
-        </a>
-        .
-      </p>
-      <h2>Help! I sent ETH to my Celo address</h2>
-      <p>
-        See this guide{' '}
-        <a
-          target="_blank"
-          href="https://docs.celo.org/celo-owner-guide/celo-recovery"
-        >
-          here
-        </a>
-        .
-      </p>
+      <Collapsible title="Help! I sent CELO to my Ethereum address">
+        <p>
+          See this guide{' '}
+          <a
+            target="_blank"
+            href="https://docs.celo.org/celo-owner-guide/eth-recovery"
+          >
+            here
+          </a>
+          .
+        </p>
+      </Collapsible>
+      <Collapsible title="Help! I sent ETH to my Celo address">
+        <h2></h2>
+        <p>
+          See this guide{' '}
+          <a
+            target="_blank"
+            href="https://docs.celo.org/celo-owner-guide/celo-recovery"
+          >
+            here
+          </a>
+          .
+        </p>
+      </Collapsible>
     </div>
   );
 }
