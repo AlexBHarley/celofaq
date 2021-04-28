27030f1e5a36baffce29b9ee1eba298d93cf949c
Alex Harley <alexbruceharley@gmail.com>
2021-04-28 22:36:56 +0200

    chore: plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,20 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Celo FAQ</title>
+        <meta name="keywords" content="Celo, blockchain" />
+        <meta
+          name="description"
+          content="Frequently asked questions about Celo"
+        />
+
+        {process.browser && (
+          <script
+            async
+            defer
+            data-domain="celofaq.com"
+            src="https://stats.celofaq.com/js/index.js"
+          />
+        )}
       </Head>
       <Component {...pageProps} />
     </>
